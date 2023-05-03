51f42b4b7caf303e829683e0ae4e8bad1a24a064
stb.infoserv <stb.infoserv@gmail.com>
2023-05-03 21:09:12 +0300

    Add project files.

---
+++ WpfDynamicResourcesTester/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDynamicResourcesTester
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Connect(string dictionary)
+        {
+            Uri uri = new Uri(dictionary, UriKind.Relative);
+            ResourceDictionary res = (ResourceDictionary)Application.LoadComponent(uri);
+            if(Application.Current.Resources.MergedDictionaries.Count>1) Application.Current.Resources.MergedDictionaries.RemoveAt(Application.Current.Resources.MergedDictionaries.Count-1);
+            Application.Current.Resources.MergedDictionaries.Add(res);
+        }
+        private void LightRB_OnChecked(object sender, RoutedEventArgs e)
+        {
+            Connect("Light.xaml");
+        }
+
+        private void DarkRB_OnChecked(object sender, RoutedEventArgs e)
+        {
+            Connect("Dark.xaml");
+        }
+    }
+}
